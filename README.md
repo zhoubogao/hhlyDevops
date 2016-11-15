3efb9741b733a6418e1b9afa7f1706954f567368
zhoubogao <121712350@qq.com>
2016-11-15 17:08:37 +0800

    Changes to be committed: 	modified:   README.md 	modified:   config.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # hhlyDevops
+部署生产的时候。在项目文件夹的上一级目录找数据库文件。
+config.py 中 配置的是：
+	DATABASE_FILE = '../hhlyDevops_db.sqlite'
+	
--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 SECRET_KEY = '123456790'
 
 # Create in-memory database
-DATABASE_FILE = 'hhlyDevops_db.sqlite'
+DATABASE_FILE = '../hhlyDevops_db.sqlite'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = True
